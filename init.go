a3ea697f472370709c88adc5f38fbab9ee40f6ee
Jonathan Barrow <jonbarrow1998@gmail.com>
2022-09-01 19:21:08 -0400

    Cleaned up init and added plogger

--- init.go
+++ init.go
@@ -1,21 +1,16 @@
 package main
 
 import (
-	"log"
-
+	"github.com/PretendoNetwork/plogger-go"
 	"github.com/joho/godotenv"
 )
 
-type Config struct {
-	Mongo struct {
-	}
-	Cassandra struct{}
-}
+var logger = plogger.NewLogger()
 
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		logger.Warning("Error loading .env file")
 	}
 
 	connectMongo()
